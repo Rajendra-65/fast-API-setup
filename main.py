cc99476cd15bdd27f4c13e8f0f85aee683a7198c
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-08-26 17:07:08 +0530

    Added the view-patient route

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI;
+from fastapi import FastAPI,Path,HTTPException;
 import json;
+
 app = FastAPI()
 
 def load_data () : 
@@ -21,9 +22,20 @@ def about() :
         'message' : "A fully functional API to manage your patient records"
     }
 
+# get route
 @app.get("/view")
 
 def view() : 
     data = load_data()
 
     return data
+
+@app.get('/patient/{patient_id}')
+
+def view_patient (patient_id : str = Path(..., description = "Id of the patient in the DB", example = "p001")) :
+    # load all patient
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code = 404 , detail = "patient not found")
