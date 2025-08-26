8dfafc95f6f7e89c4d38610028636ea4b3bc2996
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-08-26 12:22:26 +0530

    FastAPI Demo Setup

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI;
+
+app = FastAPI()
+
+@app.get("/")
+
+def hello():
+    return ("message : Hello World")
+
+@app.get("/about")
+
+def about() : 
+    return {
+        'message' : "CampusX is an education platform"
+    }
